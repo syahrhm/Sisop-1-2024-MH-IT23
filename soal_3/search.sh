bc951f2d43e95a8439fd4ff3de317b0472a16de4
v0rein <143814923+v0rein@users.noreply.github.com>
2024-03-25 18:54:25 +0700

    Add files via upload

---
+++ soal_3/search.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while true; do
+  waktu=$(date +"%Y%m%d%H%M%S")
+
+  find genshin_character/ -maxdepth 2 -type f | while read -r file; do
+    steghide extract -sf "$file" -p "" -xf extracted.txt
+    cat extracted.txt
+    decrypted_txt=$(base64 -d extracted.txt)
+    echo "$decrypted_txt"
+    if [[ $decrypted_txt == https* ]]; then
+      wget "$decrypted_txt"
+      echo "$decrypted_txt" > decrypted_url.txt
+      rm extracted.txt
+      echo "[$waktu] [FOUND] [$file]" >> image.log
+      exit 0 
+    fi
+    echo "[$waktu] [NOT FOUND] [$file]" >> image.log
+    rm extracted.txt
+  done
+
+  sleep 1
+
+  if [[ $? -eq 0 ]]; then
+    break
+  fi
+done
